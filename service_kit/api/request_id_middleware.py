82716fb419d1686b0f65d2e0de96ea1837b267c1
Mike Salvatore <msalvato@akamai.com>
2026-09-21 08:26:25 -0400

    Warn of log injection attacks
    Extracts common header retrieval logic to a static method. Logs a
    warning if a potential log injection attack is detected.

--- service_kit/api/request_id_middleware.py
+++ service_kit/api/request_id_middleware.py
@@ -4,6 +4,8 @@ from typing import Callable, Final
 from fastapi import Request, Response
 from starlette.middleware.base import BaseHTTPMiddleware
 
+from service_kit.logging import SecurityRisk, logger
+
 from . import RequestID
 
 REQUEST_ID_HEADER: Final[str] = "x-request-id"
@@ -43,21 +45,39 @@ class RequestIDMiddleware(BaseHTTPMiddleware):
     async def dispatch(
         self, request: Request, call_next: Callable[[Request], Response]
     ) -> Response:
-        request_id_from_header = request.headers.get(REQUEST_ID_HEADER, "").strip()
-        correlation_id_from_header = request.headers.get(CORRELATION_ID_HEADER, "").strip()
+        request_id_from_header = self._safe_retrieve_header(request, REQUEST_ID_HEADER)
+        correlation_id_from_header = self._safe_retrieve_header(request, CORRELATION_ID_HEADER)
 
-        # Reject IDs that contain characters outside [a-zA-Z0-9_-] to prevent log injection.
-        if request_id_from_header and _SAFE_ID_RE.match(request_id_from_header):
-            request.state.id = request_id_from_header
-        else:
-            request.state.id = self._generate_request_id()
-
-        if correlation_id_from_header and _SAFE_ID_RE.match(correlation_id_from_header):
-            request.state.correlation_id = correlation_id_from_header
-        else:
-            request.state.correlation_id = None
+        request.state.id = (
+            request_id_from_header if request_id_from_header else self._generate_request_id()
+        )
+        request.state.correlation_id = correlation_id_from_header
 
         return await call_next(request)
+
+    @staticmethod
+    def _safe_retrieve_header(request: Request, header: str) -> str | None:
+        value_from_header = request.headers.get(header)
+
+        if value_from_header is None:
+            return None
+
+        if not _SAFE_ID_RE.match(value_from_header):
+            logger.warning(
+                (
+                    "A header containing illegal characters was received. "
+                    "This may indicate a log injection attack."
+                ),
+                security_risk=SecurityRisk.LOW,
+                header=header,
+                # Logging the value is safe because the logger escapes unsafe characters when
+                # serializing to JSON
+                value=value_from_header,
+            )
+
+            return None
+
+        return value_from_header
 
     @staticmethod
     def _generate_request_id() -> RequestID:
